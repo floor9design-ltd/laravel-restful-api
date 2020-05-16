323d7dc09d4e2bf156401674a80dd4f107c6a4cf
elb98rm <rick@floor9design.com>
2020-05-16 18:02:20 +0100

    Updates to the core methods, updating methods to use the new approach to validation rules.

--- src/Traits/ApiJsonDefaultTrait.php
+++ src/Traits/ApiJsonDefaultTrait.php
@@ -193,12 +193,13 @@ trait ApiJsonDefaultTrait
      */
     public function jsonCreate(Request $request): JsonResponse
     {
-        // laravel parse the request into an array:
+        // parse the request into an array:
         $re_encoded_array = $this->extractJsonApiAttributes($request->all());
 
+        $object = new $this->controller_model();
         $validator = Validator::make(
             $re_encoded_array,
-            $this->controller_model::getValidation()
+            $object->getValidation()
         );
 
         if ($validator->fails()) {
@@ -206,7 +207,7 @@ trait ApiJsonDefaultTrait
                 $this->json_api_response_array['errors'][] = [
                     'status' => '422',
                     'title' => 'Input validation has failed',
-                    'detail' => $field_errors //$validator_error
+                    'detail' => $field_errors
                 ];
             }
 
@@ -219,7 +220,7 @@ trait ApiJsonDefaultTrait
 
             $single_object_name = Inflector::singularize($this->model->getTable());
 
-            $object = new $this->controller_model($re_encoded_array);
+            $object->fill($re_encoded_array);
             $object->save();
 
             // remember: even if the ID is not called "id", JSON API format requires that it be called that:
@@ -254,36 +255,30 @@ trait ApiJsonDefaultTrait
      */
     public function jsonCreateById(Request $request, int $id): JsonResponse
     {
-        // An often used validator rule is something like:
+        // Using an ID to specifically create an item is unusually, as people normally can't guarantee the database state
+        // This causes validator issues. An often used validator rule is something like:
         // 'id' => 'sometimes|exists:objects|integer',
-        // This fails when forcing a specific ID create, create a second validator for just the ID:
+        // This fails when forcing a specific ID create, so overwrite with a sensible one for just the ID:
 
-        // laravel parse the request into an array:
+        // parse the request into an array:
         $re_encoded_array = $this->extractJsonApiAttributes($request->all());
+        $re_encoded_array[$this->model->getKeyName()] = $id;
+        $object = new $this->controller_model();
 
-        // Run normal validation (without id)
-        $validator_standard = Validator::make(
+        $updated_validation_array = $object->getValidation();
+        $updated_validation_array[$this->model->getKeyName()] = 'sometimes|unique:' . $this->model->getTable(
+            ) . ',' . $this->model->getKeyName() . '|integer';
+
+        $validator = Validator::make(
             $re_encoded_array,
-            $this->controller_model::getValidation()
+            $updated_validation_array
         );
 
         $re_encoded_array[$this->model->getKeyName()] = (int)$id;
 
-        // Then run a second validation to check ID uniqueness:
-        $validator_id = Validator::make(
-            $re_encoded_array,
-            [
-                $this->model->getKeyName() => 'sometimes|unique:' . $this->model->getTable(
-                    ) . ',' . $this->model->getKeyName() . '|integer'
-            ]
-        );
-
-        // combine the errors of the two validators:
-        $combined_validator_errors = [];
-
-        if ($validator_standard->fails()) {
-            foreach ($validator_standard->errors()->all() as $field_errors) {
-                $combined_validator_errors[] = [
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $field_errors) {
+                $this->json_api_response_array['errors'][] = [
                     'status' => '422',
                     'title' => 'Input validation has failed',
                     'detail' => $field_errors
@@ -292,32 +287,14 @@ trait ApiJsonDefaultTrait
 
             unset($this->json_api_response_array['meta']);
             unset($this->json_api_response_array['data']);
-        }
-
-        if ($validator_id->fails()) {
-            foreach ($validator_id->errors()->all() as $id_errors) {
-                $combined_validator_errors[] = [
-                    'status' => '422',
-                    'title' => 'Input validation has failed',
-                    'detail' => $id_errors
-                ];
-            }
-
-            unset($this->json_api_response_array['meta']);
-            unset($this->json_api_response_array['data']);
-        }
-
-        if ($validator_standard->fails() || $validator_id->fails()) {
-            $this->json_api_response_array['errors'] = $combined_validator_errors;
-            unset($this->json_api_response_array['meta']);
-            unset($this->json_api_response_array['data']);
 
             $status = $this->json_api_response_array['errors'][0]['status'];
         } else {
-            $single_object_name = Inflector::singularize($this->model->getTable());
             unset($this->json_api_response_array['errors']);
 
-            $object = new $this->controller_model($re_encoded_array);
+            $single_object_name = Inflector::singularize($this->model->getTable());
+
+            $object->fill($re_encoded_array);
             $object->save();
 
             // remember: even if the ID is not called "id", JSON API format requires that it be called that:
@@ -352,32 +329,50 @@ trait ApiJsonDefaultTrait
      * @param int $id
      * @return JsonResponse json response
      */
-    public function jsonElementReplace(
-        Request $request,
-        int $id
-    ): JsonResponse {
-        // laravel issue with PUT, so create the array manually:
-        $object = new $this->controller_model();
-        foreach ($object->getFillable() as $fillable) {
-            $filtered[$fillable] = $request->get($fillable);
-        }
-        $filtered['id'] = $id;
+    public function jsonElementReplace(Request $request, int $id): JsonResponse
+    {
+        // parse the request into an array:
+        $re_encoded_array = $this->extractJsonApiAttributes($request->all());
 
-        $validator = Validator::make(
-            $filtered,
-            $this->controller_model::getValidation($object->getDefaultIgnoredUniques(), $id)
+        // Need to validate twice: once for ID:
+        $validator_id = Validator::make(
+            [$this->model->getKeyName() => $id],
+            [
+                $this->model->getKeyName() => 'exists:' . $this->model->getTable() . ',' . $this->model->getKeyName(
+                    ) . '|integer'
+            ]
         );
 
-        if ($validator->fails()) {
+        // Now we have that, load the model
+        $object = $this->controller_model::find($id);
+
+        // if there's no object, instantiate one to help with validation:
+        if (!$object) {
+            $object = new $this->model();
+        }
+
+        $validator = Validator::make(
+            $re_encoded_array,
+            $object->getValidation()
+        );
+
+        if ($validator_id->fails() || $validator->fails()) {
+            foreach ($validator_id->errors()->messages() as $error) {
+                foreach ($error as $message) {
+                    $validator->errors()->add('id', $message);
+                }
+            }
+
             $this->json_api_response_array['status'] = '422';
             $this->json_api_response_array['detail'] = 'Input validation has failed.';
             $this->json_api_response_array['validator_errors'] = $validator->errors();
         } else {
-            // Clear old object
-            $old = $this->controller_model::find($id);
-            $old->forceDelete();
+            // Delete the old one : replace, not update, and use correct method to counteract soft deletes
+            $object->forceDelete();
             // Write replacement
-            $object->fill($filtered);
+            $object = new $this->model();
+            $object->fill($re_encoded_array);
+            $object->id = $id;
             $object->save();
             $this->json_api_response_array['status'] = '200';
             $this->json_api_response_array['detail'] = 'The ' . Inflector::singularize(
@@ -394,7 +389,6 @@ trait ApiJsonDefaultTrait
     // PATCH
 
     // jsonCollectionUpdate
-
     /**
      * Replaces an element
      * "Replace the usered member of the collection, or if it does not exist, create it."
@@ -405,16 +399,21 @@ trait ApiJsonDefaultTrait
      */
     public function jsonElementUpdate(Request $request, int $id): JsonResponse
     {
-        // laravel issue with PATCH, so create the array manually:
-        $object = new $this->controller_model();
-        foreach ($object->getFillable() as $fillable) {
-            $filtered[$fillable] = $request->get($fillable);
+        // parse the request into an array:
+        $re_encoded_array = $this->extractJsonApiAttributes($request->all());
+        $re_encoded_array['id'] = $id;
+
+        // Now we have that, load the model
+        $object = $this->controller_model::find($id);
+
+        // if there's no object, instantiate one to help with validation:
+        if (!$object) {
+            $object = new $this->model();
         }
-        $filtered['id'] = $id;
 
         $validator = Validator::make(
-            $filtered,
-            $this->controller_model::getValidation($object->getDefaultIgnoredUniques(), $id)
+            $re_encoded_array,
+            $object->getValidation()
         );
 
         if ($validator->fails()) {
@@ -422,9 +421,7 @@ trait ApiJsonDefaultTrait
             $this->json_api_response_array['detail'] = 'Input validation has failed.';
             $this->json_api_response_array['validator_errors'] = $validator->errors();
         } else {
-            $object = $this->controller_model::find($id);
-
-            $object->fill($filtered);
+            $object->fill($re_encoded_array);
             $object->save();
             $this->json_api_response_array['status'] = '200';
             $this->json_api_response_array['detail'] = 'The ' . Inflector::singularize(
@@ -440,7 +437,23 @@ trait ApiJsonDefaultTrait
 
     // DELETE
 
-    // jsonCollectionDelete
+    /**
+     * Delete the entire collection.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse json response
+     */
+    public function jsonCollectionDelete(Request $request): JsonResponse
+    {
+        $this->model::query()->delete();
+
+        $this->json_api_response_array['status'] = '200';
+        $this->json_api_response_array['detail'] = 'The collection inside the ' . $this->model->getTable(
+            ) . ' was deleted.';
+
+        return Response::json($this->json_api_response_array, $this->json_api_response_array['status']);
+    }
 
     /**
      * Delete the element.
@@ -467,13 +480,13 @@ trait ApiJsonDefaultTrait
     }
 
     /**
-     * Parses an input array in the form of a JSON API request, returning thr attributes key.
+     * Parses an input array in the form of a JSON API request, returning the attributes key.
      * Also prepares json fields for database writing
      *
      * @param array $array
      * @return array
      */
-    public function extractJsonApiAttributes(array $array): Array
+    public function extractJsonApiAttributes(array $array): array
     {
         $re_encoded_array = [];
 
